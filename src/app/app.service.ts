2d9357d3d33d7d6efc13ca1ae401a9f179890f98
okolomiets <o.kolomiets@svitla.com>
2018-03-09 17:49:11 +0100

    Moved buy stocks to service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import { of } from 'rxjs/observable/of';
 import { tap, map, filter, take, switchMap, catchError } from 'rxjs/operators';
 
 import { Market } from './models/market.model';
 import { Stocks } from './models/stocks.model';
 import { User } from './models/user.model';
-import { Subject } from '../../node_modules/rxjs/Subject';
+
+import { AppDialogsService } from './shared/dialogs.service';
 
 @Injectable()
 export class AppService {
@@ -18,7 +20,10 @@ export class AppService {
   userBalance: User;
   marketsEntities: {[key: number]: Market};
   stocksEntities: {[key: number]: Stocks};
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private appDialogService: AppDialogsService
+  ) { }
 
   getBalance(): Observable<User> {
     return this.http
@@ -120,6 +125,46 @@ export class AppService {
       .delete<Stocks>(url).pipe(
         catchError((error: any) => Observable.throw(error))
       );
+  }
+
+  buyStocks(purchase) {
+    purchase.total = Number((Number(purchase.market.price) * purchase.quantity).toFixed(2));
+    purchase.lastUpdated = new Date();
+
+    if (this.userBalance.balance - purchase.total > this.userBalance.balance) {
+      this.appDialogService.showWarningSnackBar('Invalid quantity value!', 'Dismiss');
+
+    } else if (this.userBalance.balance - purchase.total > 0) {
+
+      return this.getStocksEntities().pipe(
+        map(stocksEntities => stocksEntities[purchase.market.id]),
+        switchMap((existedStocks): any => {
+          if (existedStocks) {
+
+            existedStocks.quantity += purchase.quantity;
+            existedStocks.total += purchase.total;
+            existedStocks.lastUpdated = purchase.lastUpdated;
+
+            return this.updateStocks(existedStocks).pipe(
+              tap(() => {
+                this.updateBalance(-purchase.total);
+              })
+            );
+
+          } else {
+            return this.saveStocks(purchase).pipe(
+              tap(() => {
+                this.updateBalance(-purchase.total);
+              })
+            );
+          }
+        }),
+        catchError((error: any) => Observable.throw(error))
+      );
+
+    } else {
+      this.appDialogService.showWarningSnackBar('Not enough balance to buy!', 'Dismiss');
+    }
   }
 
 }
--- src/app/components/markets/markets.component.ts
+++ src/app/components/markets/markets.component.ts
@@ -5,7 +5,6 @@ import { Subscription } from 'rxjs/Subscription';
 
 import { Market } from '../../models/market.model';
 import { AppService } from '../../app.service';
-import { AppDialogsService } from '../../shared/dialogs.service';
 
 @Component({
   selector: 'app-markets',
@@ -16,61 +15,22 @@ export class MarketsComponent implements OnInit, OnDestroy {
   displayedColumns = ['name', 'category', 'price', 'buy'];
   markets = new MarketsDataSource(this.appService);
   maxQauntity = 1000000;
-  getStocksEntitiesSub: Subscription;
-  updateStocksSub: Subscription;
-  saveStocksSub: Subscription;
+  buyStocksSub: Subscription;
 
   constructor(
-    private appService: AppService,
-    private appDialogService: AppDialogsService
+    private appService: AppService
   ) { }
 
   ngOnInit() {}
 
   ngOnDestroy() {
-    if (this.getStocksEntitiesSub) {
-      this.getStocksEntitiesSub.unsubscribe();
-    }
-    if (this.updateStocksSub) {
-      this.updateStocksSub.unsubscribe();
-    }
-    if (this.saveStocksSub) {
-      this.saveStocksSub.unsubscribe();
+    if (this.buyStocksSub) {
+      this.buyStocksSub.unsubscribe();
     }
   }
 
   buyStocks(purchase) {
-    purchase.total = Number((Number(purchase.market.price) * purchase.quantity).toFixed(2));
-    purchase.lastUpdated = new Date();
-
-    if (this.appService.userBalance.balance - purchase.total > this.appService.userBalance.balance) {
-      this.appDialogService.showWarningSnackBar('Invalid quantity value!', 'Dismiss');
-
-    } else if (this.appService.userBalance.balance - purchase.total > 0) {
-
-      this.getStocksEntitiesSub = this.appService.getStocksEntities().subscribe(stocksEntities => {
-        const existedStocks = stocksEntities[purchase.market.id];
-
-        if (existedStocks) {
-          existedStocks.quantity += purchase.quantity;
-          existedStocks.total += purchase.total;
-          existedStocks.lastUpdated = purchase.lastUpdated;
-          this.updateStocksSub = this.appService.updateStocks(existedStocks).subscribe(
-            () => {
-              this.appService.updateBalance(-purchase.total);
-            });
-
-        } else {
-          this.saveStocksSub = this.appService.saveStocks(purchase).subscribe(
-            () => {
-              this.appService.updateBalance(-purchase.total);
-            });
-        }
-      });
-
-    } else {
-      this.appDialogService.showWarningSnackBar('Not enough balance to buy!', 'Dismiss');
-    }
+    this.buyStocksSub = this.appService.buyStocks(purchase).subscribe(() => {});
   }
 
 }
